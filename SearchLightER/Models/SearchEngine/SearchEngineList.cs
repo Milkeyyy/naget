522835150a7cb04253a91c538d04b0a39158f371
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2025-03-17 14:19:27 +0900

    検索エンジン一覧の初期値を設定

--- SearchLightER/Models/SearchEngine/SearchEngineList.cs
+++ SearchLightER/Models/SearchEngine/SearchEngineList.cs
@@ -8,7 +8,11 @@ public class SearchEngineList
 	public SearchEngineList()
 	{
 		CreatedAt = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToLongTimeString();
-		List = [];
+		List = [
+			new SearchEngineClass("Bing", "https://www.bing.com/search?form=&q={0}", "Bing"),
+			new SearchEngineClass("DuckDuckGo", "https://duckduckgo.com/?q={0}", "DuckDuckGo"),
+			new SearchEngineClass("Google", "https://www.google.com/search?q={0}", "Google")
+		];
 	}
 
 	public string CreatedAt { get; set; }
