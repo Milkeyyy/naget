30a6c6836949937484d286eba1acd88b1c263dee
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2025-06-22 13:25:41 +0900

    [Settings.SearchView] 検索エンジン削除コマンドを実装

--- SearchLightER/ViewModels/Settings/SearchViewModel.cs
+++ SearchLightER/ViewModels/Settings/SearchViewModel.cs
@@ -8,6 +8,7 @@ using naget.Views;
 using naget.Views.Settings;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace naget.ViewModels.Settings;
@@ -16,25 +17,54 @@ namespace naget.ViewModels.Settings;
 public class SearchViewModel
 {
 	public Well<UserControl> SearchViewWell { get; } = Well.Factory.Create<UserControl>();
-	
-	public static ReadOnlyCollection<SearchEngineClass> SearchEngineList { get { return SearchEngineManager.EngineList; } }
 
-	public Command AddSearchEngineCommand { get; }
+	public ReadOnlyCollection<SearchEngineViewModel> SearchEngineList { get; private set; }
+
+	public Command CreateSearchEngineCommand { get; }
+	public Command ReloadSearchEngineCommand { get; }
 
 	public SearchViewModel()
 	{
+		SearchEngineList = new([]);
+
 		// ビューがロードされた時の処理
 		SearchViewWell.Add(Control.LoadedEvent, () =>
 		{
 			Debug.WriteLine("SearchView Loaded");
 			Debug.WriteLine(SearchEngineList);
+			LoadSearchEngineList();
 			return default;
 		});
 
-		AddSearchEngineCommand = Command.Factory.Create(async () =>
+		// 検索エンジンの作成コマンド
+		CreateSearchEngineCommand = Command.Factory.Create(async () =>
 		{
 			await ShowInputDialogAsync();
 		});
+
+		// 検索エンジンの削除コマンド
+		ReloadSearchEngineCommand = Command.Factory.Create(() =>
+		{
+			Debug.WriteLine("Delete Search Engine Command");
+			LoadSearchEngineList();
+			return default;
+		});
+	}
+
+	public void LoadSearchEngineList()
+	{
+		Debug.WriteLine("Load Search Engine List");
+		// 検索エンジンのリストを初期化する
+		SearchEngineList = new(SearchEngineManager.EngineList.Select(v => new SearchEngineViewModel(v, ReloadSearchEngineCommand)).ToList());
+	}
+
+	[PropertyChanged(nameof(SearchEngineList))]
+	private ValueTask SearchEngineListChangedAsync(ReadOnlyCollection<SearchEngineClass> value)
+	{
+	 	Debug.WriteLine("Search Engine List Changed");
+		// 検索エンジン一覧を読み込み直す
+		LoadSearchEngineList();
+		return default;
 	}
 
 	public async Task ShowInputDialogAsync()
@@ -58,24 +88,56 @@ public class SearchViewModel
 			//PrimaryButtonCommand = command
 		};
 
+		// ダイアログを表示する
 		var result = await dialog.ShowAsync();
 
 		if (result == ContentDialogResult.Primary)
 		{
-			Debug.WriteLine("HotKey Preset Create Dialog - User clicked Create");
+			Debug.WriteLine("Search Engine Create Dialog - User clicked Create");
 			if (string.IsNullOrWhiteSpace(vm.Name) || string.IsNullOrWhiteSpace(vm.Url))
 			{
-				Debug.WriteLine("HotKey Preset Create Dialog - Preset Name or URL is empty");
+				Debug.WriteLine("Search Engine Create Dialog - Preset Name or URL is empty");
 				await SuperDialog.Info(App.SettingsWindow, Resources.Settings_ShortcutKey_Preset_CreateNewPreset, Resources.Settings_ShortcutKey_Preset_NameIsEmpty);
 				await ShowInputDialogAsync();
 				return;
 			}
 			// プリセットを作成する
 			SearchEngineManager.Create(vm.Name, vm.Url);
+
+			// 検索エンジン一覧を読み込み直す
+			LoadSearchEngineList();
 		}
 		else
 		{
-			Debug.WriteLine("HotKey Preset Create Dialog - User clicked Cancel");
+			Debug.WriteLine("Search Engine Create Dialog - User clicked Cancel");
 		}
+	}
+}
+
+[ViewModel]
+public class SearchEngineViewModel
+{
+	public string Id { get; set; } = string.Empty;
+	public string Name { get; set; } = string.Empty;
+	public string Uri { get; set; } = string.Empty;
+
+	public Command DeleteSearchEngineCommand { get; }
+	public Command ReloadSearchEngineCommand { get; }
+
+	public SearchEngineViewModel(SearchEngineClass searchEngine, Command reloadCommand)
+	{
+		Id = searchEngine.Id;
+		Name = searchEngine.Name;
+		Uri = searchEngine.Uri;
+		ReloadSearchEngineCommand = reloadCommand;
+
+		// 検索エンジンの削除コマンド
+		DeleteSearchEngineCommand = Command.Factory.Create(() =>
+		{
+			Debug.WriteLine($"Delete Search Engine: {Name} ({Id})");
+			SearchEngineManager.Delete(Id);
+			ReloadSearchEngineCommand.Execute(null);
+			return default;
+		});
 	}
 }
