20624cead50a154c657860ac519c6b62f834b263
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2025-06-23 16:43:48 +0900

    汎用ダイアログを `TaskDialog` から `ContentDialog` へ変更

--- SearchLightER/Views/SuperDialog.cs
+++ SearchLightER/Views/SuperDialog.cs
@@ -1,4 +1,4 @@
-﻿using Avalonia;
+﻿using Avalonia.Controls;
 using FluentAvalonia.UI.Controls;
 using System.Threading.Tasks;
 
@@ -6,18 +6,21 @@ namespace naget.Views;
 
 public static class SuperDialog
 {
-	private static readonly TaskDialog dialog = new();
-
-	public static async Task Info(Visual root, string title, string content)
+	public static async Task Info(Window root, string title, string content)
 	{
-		dialog.XamlRoot = root;
-		dialog.VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center;
-		dialog.FontSize = 16;
-		dialog.FontWeight = Avalonia.Media.FontWeight.SemiBold;
-		dialog.Title = title;
-		dialog.Content = content;
-		dialog.Buttons = [TaskDialogButton.OKButton];
-		await dialog.ShowAsync();
+		ContentDialog dialog = new()
+		{
+			//HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
+			//VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center,
+			//FontSize = 16,
+			//FontWeight = Avalonia.Media.FontWeight.SemiBold,
+			Title = title,
+			Content = content,
+			CloseButtonText = "OK",
+			IsPrimaryButtonEnabled = false,
+			IsSecondaryButtonEnabled = false
+		};
+		await dialog.ShowAsync(root);
 	}
 
 }
