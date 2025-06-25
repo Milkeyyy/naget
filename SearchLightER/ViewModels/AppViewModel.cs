084d50da0c5e2f3d8b405184aba6eb3da7286392
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2025-06-26 02:38:26 +0900

    アプリ内ブラウザーが既に開かれている場合に通知領域のメニューから開いた際にウィンドウを最前面に移動するようにした

--- SearchLightER/ViewModels/AppViewModel.cs
+++ SearchLightER/ViewModels/AppViewModel.cs
@@ -13,25 +13,41 @@ public class AppViewModel
 
 	public AppViewModel()
 	{
+		#region コンテキストメニューの各コマンド
+		// アプリ内ブラウザー
 		OpenInAppBrowserWindowCommand = Command.Factory.Create(() =>
 		{
+			// 既にブラウザーが開いている場合はフォーカス(アクティブに)する
+			if (App.BrowserWindow.IsVisible)
+			{
+				// 一時的に最前面に設定して無理やり手前に持ってくる
+				App.BrowserWindow.Topmost = true;
+				App.BrowserWindow.Focus(); // フォーカスできないけど一応実行する
+				App.BrowserWindow.Topmost = false;
+				return default;
+			}
+			// スタートページを開いてウィンドウを表示する
 			(App.BrowserWindow.DataContext as BrowserWindowViewModel).CurrentAddress = ConfigManager.Config.BrowserWindow.StartPage;
 			App.BrowserWindow.Show();
 			return default;
 		});
+		// 設定画面
 		OpenSettingsWindowCommand = Command.Factory.Create(() =>
 		{
 			App.SettingsWindow.Show();
 			return default;
 		});
+		// アップデートチェック
 		CheckUpdateCommand = Command.Factory.Create(async () =>
 		{
 			await App.ManualUpdateCheck();
 		});
+		// アプリケーション終了
 		ExitAppCommand = Command.Factory.Create(() =>
 		{
 			App.Exit();
 			return default;
 		});
+		#endregion
 	}
 }
