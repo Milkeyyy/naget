d24c31cb4645e59e5ec2bf2b6cd74d8116ab1352
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2025-12-06 23:53:24 +0900

    フォントの置き換えを一時的に無効化

--- naget/Program.cs
+++ naget/Program.cs
@@ -21,13 +21,14 @@ namespace naget
 			=> AppBuilder.Configure<App>()
 				.UsePlatformDetect()
 				.UseSkia()
-				.With(new FontManagerOptions
-				{
-					DefaultFamilyName = "avares://naget/Assets/Fonts#Noto Sans JP",
-					FontFallbacks = [
-						new FontFallback { FontFamily = new FontFamily("avares://naget/Assets/Fonts#Noto Sans JP") }
-					]
-				})
+				// フォントの設定
+				// .With(new FontManagerOptions
+				// {
+				// 	DefaultFamilyName = "avares://naget/Assets/Fonts#Noto Sans JP",
+				// 	FontFallbacks = [
+				// 		new FontFallback { FontFamily = new FontFamily("avares://naget/Assets/Fonts#Noto Sans JP") }
+				// 	]
+				// })
 				.With(new MacOSPlatformOptions() { ShowInDock = false }) // (macOS) Dock に表示しない
 				.LogToTrace();
 	}
