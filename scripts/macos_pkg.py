da718eccabae07101fa9a61a2ea044823640b8b9
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2025-12-10 00:45:24 +0900

    Update macos_pkg.py

--- scripts/macos_pkg.py
+++ scripts/macos_pkg.py
@@ -3,16 +3,16 @@ from macos_pkg_builder import Packages
 
 args = sys.argv
 
-runtime = args[0]
-version = args[1]
+runtime = args[1]
+version = args[2]
 
 pkg_obj = Packages(
-	pkg_output="naget.pkg",
-	pkg_bundle_id="com.milkeyyy.naget",
-	pkg_version=version,
-	pkg_file_structure={
-		f"_Pack/{runtime}/naget.app": "/Applications/naget.app",
-	},
+    pkg_output="naget.pkg",
+    pkg_bundle_id="com.milkeyyy.naget",
+    pkg_version=version,
+    pkg_file_structure={
+        f"_Pack/{runtime}/naget.app": "/Applications/naget.app",
+    },
 )
 
 assert pkg_obj.build() is True
